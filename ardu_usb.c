8f2740080b0c992ee6d6997415e04df181a6cf37
jkyunj <jkyunjang@gmail.com>
2023-11-13 20:24:11 +0900

    feat: add code to make device file automatically in /dev

--- ardu_usb.c
+++ ardu_usb.c
@@ -3,6 +3,8 @@
 #include <linux/usb.h>
 #include <linux/kdev_t.h>
 #include <linux/fs.h>
+#include <linux/err.h>
+#include <linux/device.h>
 
 #define USB_VENDOR_ID (0x2341)
 #define USB_PRODUCT_ID (0x0043)
@@ -13,7 +15,7 @@
                 pr_info("bLength: 0x%x\n", i.bLength);                       \
                 pr_info("bDescriptorType: 0x%x\n", i.bDescriptorType);       \
                 pr_info("bInterfaceNumber: 0x%x\n", i.bInterfaceNumber);     \
-                pr_info("bAlternateSetting: 0x%x\n", i.bAlternateSetting);   \
+               pr_info("bAlternateSetting: 0x%x\n", i.bAlternateSetting);   \
                 pr_info("bNumEndpoints: 0x%x\n", i.bNumEndpoints);           \
                 pr_info("bInterfaceClass: 0x%x\n", i.bInterfaceClass);       \
                 pr_info("bInterfaceSubClass: 0x%x\n", i.bInterfaceSubClass); \
@@ -35,6 +37,7 @@
         }
 
 dev_t dev = 0;
+static struct class *dev_class;
 
 static int ardu_usb_probe(struct usb_interface *interface,
                           const struct usb_device_id *id)
@@ -83,9 +86,24 @@ static int __init ardu_usb_init(void)
 		return -1;
 	}
 
+	dev_class = class_create(THIS_MODULE, "ardu_usb");
+	if(IS_ERR(dev_class)) {
+		pr_err("cannot create the struct class for device\n");
+		goto err_class;
+	}
+
+	if(IS_ERR(device_create(dev_class, NULL, dev, NULL, "ardu_dev"))) {
+		pr_err("Cannot create the ardu_dev\n");
+		goto err_device;
+	}
 	pr_info("ardu_usb is loaded successfully\n");
-	
 	return 0;
+
+err_device: 
+	class_destroy(dev_class);
+err_class:
+	unregister_chrdev_region(dev, 1);
+	return -1;
 }
 
 static void __exit ardu_usb_exit(void)
@@ -100,4 +118,4 @@ module_exit(ardu_usb_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jaekyun Jang <jaegun0103@ajou.ac.kr>");
 MODULE_DESCRIPTION("arduino device driver to connect with usb");
-MODULE_VERSION("1.0.1");
+MODULE_VERSION("1.0.2");
