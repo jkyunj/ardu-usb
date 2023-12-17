6d4b24fa93a1a3f0e581a908c802f477cf5f74bb
jkyunj <jkyunjang@gmail.com>
2023-12-17 21:41:12 +0900

    Update: graph time

--- read.py
+++ read.py
@@ -1,29 +1,38 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import numpy as np
 from itertools import count
 
 fig, ax = plt.subplots() 
 
 x_data = [] # time
 y_data = [] # sensor value
+avg = [] # average of sensor value
 
 index = count()
-with open('/dev/ardu0', 'r') as f:
-    f.readline().strip() # discard first data
-    def animate(i):
-        x_data.append(next(index))
-        _f = f.readline().strip()
 
-        y_data.append(_f)
+with open('/dev/ardu0', 'r') as f:
+    def animate(i):
+        _f = f.readline().strip()
+        # read only 10 ~ 99 cm
+        if(len(_f) != 2):
+            return
+
+        x_data.append(next(index))
+        y_data.append(int(_f))
+        avg.append(np.mean(y_data))
 
         plt.cla()
-        plt.plot(x_data, y_data)
+        plt.plot(x_data, y_data, label='distance')
+        plt.plot(x_data, avg, label='average')
+        plt.legend()
+        plt.title('Ultrasonic sensor value', fontweight ="bold") 
+        plt.xlabel('Time(ms)')
+        plt.ylabel('Value(cm)')
 
-    ani = FuncAnimation(plt.gcf(), animate, interval = 1000)
-    
-    plt.title('Ultrasonic sensor value', fontweight ="bold") 
-    plt.xlabel('Time(s)')
-    plt.ylabel('Value(cm)')
-    plt.tight_layout()
+    f.readline().strip() # discard first data
+
+    ani = FuncAnimation(plt.gcf(), animate, interval = 1)
+
     plt.show()
 
